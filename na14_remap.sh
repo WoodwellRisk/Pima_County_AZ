d269e79c00d7d4025cae4f51029f13634f79530c
kngassert <kgassert@woodwellclimate.org>
2024-04-04 16:20:25 +0000

    minor updates

--- na14_remap.sh
+++ na14_remap.sh
@@ -1,6 +1,9 @@
-# remap na14 netcdfs to resolution of community precipitation data
-for s in sw*.nc; do cdo -z zip -remapbil,PimaCo_pr_CanESM5_ssp245_1971-2100_ds_bil_daymet_then_ba.nc "$s" "$s"_remapbil.nc; done &
+# specify name of file with desired grid, to be used to remap na14 data
+grid_file=PimaCo_pr_CanESM5_ssp585_1971-2100_ds_bil_daymet_then_ba.nc
+
+# remap na14 netcdfs to resolution of grid_file
+for s in sw*.nc; do cdo -z zip -remapbil,"$grid_file" "$s" "$s"_remapbil.nc; done &
 
 # rename remapped files
-mmv "sw*.nc_remapbil.nc" "PimaCo_sw#1_remapbil.nc"
+mmv "sw*.nc_remapbil.nc" "sw#1_remapbil.nc"
 
